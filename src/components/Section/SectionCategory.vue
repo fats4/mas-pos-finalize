f210acd4ffa8fb84033ca4b30644a6e2c7b55bd5
fats4 <muhammadfatu.2@gmail.com>
2024-06-28 12:06:06 +0700

    Section Category

--- src/components/Section/SectionCategory.vue
+++ src/components/Section/SectionCategory.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class=" p-4 flex px-14 max-sm:text-xs">
+    <div class="p-4 flex px-14 max-sm:text-xs">
         <h2 class="text-lg font-bold mr-4">Category</h2>
         <ul class="flex flex-wrap gap-3 items-center">
-            <li v-for="category in filteredCategories" :key="category.id">
+            <li v-for="category in displayedCategories" :key="category.id">
                 <button @click="filterByCategory(category.id)" class="text-blue-500 hover:underline">
                     {{ category.name }}
                 </button>
@@ -11,25 +11,39 @@
                 <button @click="filterByCategory(null)" class="text-blue-500 hover:underline">All</button>
             </li>
         </ul>
+        <button @click="toggleCategoryView" class="ml-auto text-blue-500 hover:underline pl-5">
+            {{ showAllCategories ? 'Show Filtered' : 'Show All' }}
+        </button>
     </div>
 </template>
 
+
 <script setup>
-import { computed, onMounted } from 'vue'
+import { ref, computed, onMounted } from 'vue'
 import { useCatgStore } from '../../stores/category'
 
 
 const categStore = useCatgStore()
+const showAllCategories = ref(false)
+
 
 onMounted(async () => {
     await categStore.fetchCategories()
 })
+
+const toggleCategoryView = () => {
+    showAllCategories.value = !showAllCategories.value
+}
 
 const filterByCategory = (categoryId) => {
     categStore.setSelectedCategory(categoryId)
 }
 
 const filteredCategories = computed(() => {
-    return categStore.categories.filter(category => category.name === 'Makanan' || category.name === 'Minuman' || category.name === 'Jajanan')
+    return categStore.categories.filter(category => category.name === 'Makanan' || category.name === 'Minuman' || category.name === 'Jajanan' || category.name === 'Dessert')
+})
+
+const displayedCategories = computed(() => {
+    return showAllCategories.value ? categStore.categories : filteredCategories.value
 })
 </script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,7 +9,8 @@
         <div class="pt-20 px-14"> <!-- Add padding to avoid content being hidden behind the navbar -->
             <SectionProduct />
             <div class="fixed bottom-0 right-0 flex justify-end p-4">
-                <button class="bg-blue-500 text-white font-bold py-2 px-4 rounded shadow-lg">
+                <button @click="navigateTo('/payment')"
+                    class="bg-blue-500 text-white font-bold py-2 px-4 rounded shadow-lg">
                     Total Bill: {{ formatRupiah(totalBill) }}
                 </button>
             </div>
@@ -26,6 +27,13 @@ import SectionProduct from '../components/Section/SectionProduct.vue'
 import SectionWrapper from '../components/Section/SectionWrapper.vue'
 import { formatRupiah } from '../utils/formatRp'
 import { useProductStore } from '../stores/product'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+const navigateTo = (route) => {
+    router.push(route)
+}
 
 const productStore = useProductStore()
 
